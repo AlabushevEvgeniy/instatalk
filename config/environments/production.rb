2c7adea777127cb3eb8987287a92f11e590c503e
AlabushevEvgeniy <alabushev-evgeniy@yandex.ru>
2019-06-21 12:43:54 +0500

    it is ready for deploying to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,11 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://toy-chat.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [
+    'http://toy-chat.herokuapp.com/cable',
+    'https://toy-chat.herokuapp.com/cable'
+    ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
